a0423f74f1c13149898d8888397498170c17cb38
samin akbari <saminakbari.e@gmail.com>
2024-08-12 13:58:50 +0330

    fix: make tables in templates of v1

--- ToDoList/my_app/urls.py
+++ ToDoList/my_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from my_app.views import get_task, register_user
+from my_app.views import get_task
 from my_app.views.add_shared_tasks_view import add_shared_tasks
 from my_app.views.create_list_view import create_list
 from my_app.views.create_task_view import create_task
@@ -9,8 +9,6 @@ from my_app.views.delete_task_view import delete_task
 from my_app.views.edit_list_view import edit_to_do_list
 from my_app.views.edit_task_view import edit_task
 from my_app.views.get_list_view import get_list
-from my_app.views.login_view import login_user
-from my_app.views.register_login_view import register_or_login
 from my_app.views.share_task_view import share_task
 from my_app.views.show_all_lists_view import show_all_lists
 
@@ -24,9 +22,6 @@ v1_urlpatterns = [
     path("task/edit/<int:task_id>/<int:list_id>/", edit_task),
     path("task/delete/<int:task_id>/<int:list_id>/", delete_task),
     path("task/get/<int:task_id>/", get_task),
-    # path("user/register/", register_user),
-    # path("user/login/", login_user),
     path("task/share/<int:task_id>/", share_task),
     path("task/shared-tasks/<int:list_id>/", add_shared_tasks),
-    path("to-do-list/app/opening/", register_or_login),
 ]
