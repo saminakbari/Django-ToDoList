93c7be857b6b6bd85d4fa5f3d93951ca891c90f3
samin akbari <saminakbari.e@gmail.com>
2024-08-13 16:09:33 +0330

    feat: add default values and other parameters to task fields

--- ToDoList/ToDoListApp/models/task2.py
+++ ToDoList/ToDoListApp/models/task2.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+from sqlite3 import Date
+
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -14,13 +17,25 @@ def get_priority(priority_number):
 
 class Task2(models.Model):
     PRIORITY_CHOICES = (('1', 'High'), ('2', 'Medium'), ('3', 'Low'))
-    title = models.CharField(max_length=100)
-    description = models.CharField(max_length=1000, )
-    deadline = models.DateField()
-    priority = models.CharField(max_length=1, choices=PRIORITY_CHOICES)
-    to_do_lists = models.ManyToManyField(to='ToDoListApp.ToDoList2', related_name='tasks')
-    users_shared_with = models.ManyToManyField(to=User, related_name='tasks_shared_with_user')
-    owner = models.ForeignKey(to=User, on_delete=models.SET_NULL, null=True)
+
+    title = models.CharField(max_length=100, verbose_name='عنوان',
+                             null=False, default="new-task", validators=)
+    description = models.CharField(max_length=1000, verbose_name='توضیحات',
+                                   null=False, default="", validators=)
+    deadline = models.DateField(max_length=10, verbose_name='زمان سرسید',
+                                null=False, default=datetime.now().date(), validators=)
+    priority = models.CharField(max_length=1, choices=PRIORITY_CHOICES,
+                                verbose_name='اولویت', null=False, default='2',
+                                validators=)
+    to_do_lists = models.ManyToManyField(to='ToDoListApp.ToDoList2', related_name='tasks',
+                                         verbose_name='لیست‌ها', null=True, validators=)
+    users_shared_with = models.ManyToManyField(to=User, related_name='tasks_shared_with_user',
+                                               verbose_name='افرادی که تسک با آنها به اشتراک گذاشته شده',
+                                               null=True, validators=)
+    owner = models.ForeignKey(to=User, on_delete=models.SET_NULL, verbose_name='صاحب',
+                              null=True, validators=)
 
     def __str__(self):
         return "task: " + self.title
+
+
