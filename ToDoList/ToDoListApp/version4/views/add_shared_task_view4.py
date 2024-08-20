9d45e3125391be0a6db1cbfb45fb8c9cd17c1ff3
samin akbari <saminakbari.e@gmail.com>
2024-08-20 15:39:57 +0330

    feat: add adding shared task to v4

--- ToDoList/ToDoListApp/version4/views/add_shared_task_view4.py
+++ ToDoList/ToDoListApp/version4/views/add_shared_task_view4.py
@@ -1,17 +1,22 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from rest_framework import generics
+from rest_framework.response import Response
 
-from ToDoListApp.models import ToDoList
+from ToDoListApp.models import ToDoList, Task
 from ToDoListApp.serializers import ToDoListSerializer
 
 
 class AddSharedTask4(LoginRequiredMixin, generics.RetrieveUpdateAPIView):
-    pass
-    # serializer_class = ToDoListSerializer
-    # lookup_field = 'id'
-    #
-    # def get_queryset(self):
-    #     return ToDoList.objects.filter(owner=self.request.user)
-    #
-    # def perform_update(self, serializer):
-    #     task_id = self.request.data['task_id']
+
+    serializer_class = ToDoListSerializer
+    lookup_field = 'id'
+
+    def get_queryset(self):
+        return ToDoList.objects.filter(owner=self.request.user)
+
+    def perform_update(self, serializer):
+        task_id = self.request.data['task_id']
+        task = Task.objects.get(pk=task_id)
+        to_do_list = self.get_object()
+        to_do_list.tasks.add(task)
+        return Response("Added.")
