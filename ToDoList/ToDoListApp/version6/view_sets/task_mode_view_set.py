5cf971021598638bb36939f8e5defdb9b7e00beb
samin akbari <saminakbari.e@gmail.com>
2024-08-23 12:31:49 +0330

    feat: add geting tasks feature to v6

--- ToDoList/ToDoListApp/version6/view_sets/task_mode_view_set.py
+++ ToDoList/ToDoListApp/version6/view_sets/task_mode_view_set.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 
-from ToDoListApp.models import Task
+from ToDoListApp.models import Task, ToDoList
 from ToDoListApp.serializers import TaskSerializer
 
 
@@ -9,6 +10,10 @@ class TaskModelViewSet(LoginRequiredMixin, viewsets.ModelViewSet):
     serializer_class = TaskSerializer
 
     def get_queryset(self):
+        if self.action == 'list':
+            user_lists = self.request.user.to_do_lists
+            to_do_list = get_object_or_404(user_lists, pk=self.request.POST['list_id'])
+            return to_do_list.tasks.all()
         return Task.objects.filter(owner=self.request.owner)
 
     def perform_create(self, serializer):
@@ -19,4 +24,4 @@ class TaskModelViewSet(LoginRequiredMixin, viewsets.ModelViewSet):
         task.to_do_lists.add(to_do_list)
         return super(TaskModelViewSet, self).perform_create(serializer)
 
-    
+
