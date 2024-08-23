f0731b51f0cbc03dae4c1309562a4075b5cde8b2
samin akbari <saminakbari.e@gmail.com>
2024-08-23 15:16:44 +0330

    fix: add messages to template where missing

--- ToDoList/ToDoListApp/forms/list_form.py
+++ ToDoList/ToDoListApp/forms/list_form.py
@@ -1,5 +1,7 @@
 from django import forms
 
+from ToDoListApp.models.task import validate_title
+
 
 class ListForm(forms.Form):
-    title = forms.CharField(label="Title: ", max_length=100, required=False)
+    title = forms.CharField(label="Title: ", max_length=100, required=False, validators=[validate_title])
