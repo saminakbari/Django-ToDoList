2b2066d948a623e31d0294f38f5154e405cd56b8
samin akbari <saminakbari.e@gmail.com>
2024-08-28 12:51:26 +0330

    fix: make add shared task apiview

--- ToDoList/ToDoListApp/version4/urls.py
+++ ToDoList/ToDoListApp/version4/urls.py
@@ -18,22 +18,22 @@ from ToDoListApp.version4.views import (
 v4_urlpatterns = [
     path("to-do-list/show-all/", ShowLists.as_view(), name="v4-show-lists"),
     path("to-do-list/create/", CreateList.as_view(), name="v4-create-list"),
-    path("to-do-list/<int:id>/edit/", EditList.as_view(), name="v4-edit-list"),
-    path("to-do-list/<int:id>/delete/", DeleteList.as_view(), name="v4-delete-list"),
-    path("to-do-list/<int:id>/get/", GetList.as_view(), name="v4-get-list"),
+    path("to-do-list/<int:pk>/edit/", EditList.as_view(), name="v4-edit-list"),
+    path("to-do-list/<int:pk>/delete/", DeleteList.as_view(), name="v4-delete-list"),
+    path("to-do-list/<int:pk>/get/", GetList.as_view(), name="v4-get-list"),
     path(
-        "to-do-list/<int:id>/task/create/", CreateTask.as_view(), name="v4-create-task"
+        "to-do-list/<int:pk>/task/create/", CreateTask.as_view(), name="v4-create-task"
     ),
-    path("task/<int:id>/edit/", EditTask.as_view(), name="v4-edit-task"),
-    path("task/<int:id>/get/", GetTask.as_view(), name="v4-get-task"),
+    path("task/<int:pk>/edit/", EditTask.as_view(), name="v4-edit-task"),
+    path("task/<int:pk>/get/", GetTask.as_view(), name="v4-get-task"),
     path(
-        "to-do-list/<int:list_id>/task/<int:id>/delete/",
+        "to-do-list/<int:list_id>/task/<int:pk>/delete/",
         DeleteTask.as_view(),
         name="v4-delete-task",
     ),
-    path("task/<int:id>/share/", ShareTask.as_view(), name="v4-share-task"),
+    path("task/<int:pk>/share/", ShareTask.as_view(), name="v4-share-task"),
     path(
-        "to-do-list/<int:id>/add-shared-tasks/",
+        "to-do-list/<int:pk>/add-shared-tasks/",
         AddSharedTask.as_view(),
         name="v4-add-shared-tasks",
     ),
--- ToDoList/ToDoListApp/version4/views/add_shared_task_view.py
+++ ToDoList/ToDoListApp/version4/views/add_shared_task_view.py
@@ -1,21 +1,14 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from rest_framework.generics import RetrieveUpdateAPIView
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from ToDoListApp.models import Task, ToDoList
-from ToDoListApp.serializers import ToDoListSerializer
 
 
-class AddSharedTask(LoginRequiredMixin, RetrieveUpdateAPIView):
-    serializer_class = ToDoListSerializer
-    lookup_field = "id"
-
-    def get_queryset(self):
-        return ToDoList.objects.filter(owner=self.request.user)
-
-    def perform_update(self, serializer):
+class AddSharedTask(LoginRequiredMixin, APIView):
+    def post(self, request):
         task_id = self.request.data["task_id"]
         task = Task.objects.get(pk=task_id)
-        to_do_list = self.get_object()
+        to_do_list = ToDoList.objects.get(pk=self.kwargs['pk'])
         to_do_list.tasks.add(task)
-        return Response("Added.")
+        return Response("Task added successfully.", status=200)
